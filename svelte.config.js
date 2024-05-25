842a8eef60a304fbc9bf96ddff317c9ae2342b5e
craig-rtg <craig.mlambo@rtg.co.zw>
2024-05-25 11:44:10 +0200

    Vercel update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 
 // /** @type {import('@sveltejs/kit').Config} */
@@ -20,21 +20,47 @@ import adapter from '@sveltejs/adapter-static';
 
 // export default config;
 
+
+
+
+
+
+
+import adapter from '@sveltejs/adapter-vercel';
+
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		prerender: {
-			// Exclude pages with actions from prerendering
-			entries: ['/', '/about-us', '/donate','/events','/news','/contact-us',]
-		}
-	},
-	preprocess: vitePreprocess()
+			// see below for options that can be set here
+		})
+	}
 };
+
+
+
+
+
+
+
+
+
+
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown. On some platforms
+// 			// these options are set automatically — see below
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: undefined,
+// 			precompress: false,
+// 			strict: true
+// 		}),
+// 		prerender: {
+// 			// Exclude pages with actions from prerendering
+// 			entries: ['/', '/about-us', '/donate','/events','/news','/contact-us',]
+// 		}
+// 	},
+// 	preprocess: vitePreprocess()
+// };
